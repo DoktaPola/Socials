60b7b439cd6ef871e69e7a748fa1c515e38764ae
andryjkost <61208923+andryjkost@users.noreply.github.com>
2020-05-15 13:19:24 +0300

    icon
    добавим иконку

--- design.py
+++ design.py
@@ -5,13 +5,105 @@
 # Created by: PyQt5 UI code generator 5.14.1
 #
 # WARNING! All changes made in this file will be lost!
-
+from wx.lib.embeddedimage import PyEmbeddedImage
 
 from PyQt5 import QtCore, QtGui, QtWidgets
+hse = PyEmbeddedImage(
+    b'iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAEqhJ'
+    b'REFUaIGdmnt8VOWZx7/vucw9yeTK5GogCQkBRCAoiOIF1MLaar1td/2srm2t1tZ2192P26Vu'
+    b'd7fa1rb2rp9qtVvv10qtrYooilxFCCIQSAK5E3KdySSZzPWc8+4fZzIQSRT7JPP5zJzzznue'
+    b'3/vcn2eElFLyGchKL1eEACAcTdJ0PEzHUISxmIElJU5Nwe3QUBV7TcqwiCYNTAmqEOT5HNTM'
+    b'ymJeaQ4uXZ123zMlcaYApASJRBECKSUHj4dp7AhhmpKKAg9zinyU5HrwOLSZ9wDGYimOBydo'
+    b'G4jQPxoj26OzvKqAykJfBogQgjOFcUYALCkRCISAxo4gO48OUez3sGZBAL/HgQSktMHt6wyh'
+    b'KoJFFbmkTAtdVRiNJtlyZICrlpYjJUwespSSo/3j7GgdImGYXLGwhNlFvs8mDfkpZJiWlFLK'
+    b'vnBUPvRWs3xhV4cciyWllFIe6A7J7uGIlFLKpGFKKaV85O1WeccfPpBSSpky7O++vr9XXv6D'
+    b't6Vlf5SmZUnDNKVlWTJ9SQ6OxeQjm1vl09vbZTSRsteZk3dnJuWTwJmWRFUEW5sHeGZHB59f'
+    b'XMYNyyvJculs2NPNiXCMB14/Qk8oiq4qGKaF36NTPcvHi+93oqmCobE4R06Mcl51PjtbBwHo'
+    b'DUX5x4d2kTItBLCvM0gokuRrl9YwrzSHBze10NI3iqIITOuTFWRahbVVAlRF8PSOdpBw19p5'
+    b'iLRIQ5EETZ0j/Nf1i0jGTT5sG6Y8r4Kh8QQup8Ktl1bTemIss9cXG8qQEtoGxgHI9zm59/qF'
+    b'6Kp9finTYmvzALGkwZLKPKpnZfH41mP0hmJcOj+QOcjpaFobmNTThze3MrvIxxULS2jsCBLI'
+    b'cVGa5wXg12+2kDItSvxOLj+7lHyfM8PMwGiMSMKkPxzDpav4PQ5yvTqzctxTnmNYkoPdIXJ9'
+    b'Dn7+12ZW1RVx3fKzMEwLTVV4Yls7OR6Nq5dWzAjiNABW2hgf3txKXUk2F88L8Oq+HrqHJ7jp'
+    b'wiqy3XqG0Z5glLMKPIzFUry67wSv7e9jb0eYE+EYiZhhnwSAquBx61QWuDm/Jp+rlpRwSX0R'
+    b'XqfOK409TCRS9ARjfOOyuWS5dCwpiSUNvE6dZ3Z2kOXU+MLS8mlBTAEwyfzTO9rJ8zlZt6gU'
+    b'gM1Nfdy3oYlrzyvH41S5amkZ+T4XwUiC+//SzGNbOggHowgEUldBUxAKTDpDS0qwJBgWImUi'
+    b'FUFleTbfuqyKb1xWg2nZEp+MCRMJg/teOcCtl9QwpyiLx949xtziLFbVzcrweBqAyRtbjgzQ'
+    b'NRzh5gur0kYm0FTB7945xolwlDsvn0u+z8lDbx9l/fOHGAvFER4dRVcQaT4lkvT/yQcJG5Ci'
+    b'pNckTGTCYHZlDo9+ZSmr5weQEkZjSe58vJEsl8bF8ws5v6aQsjwvP3v9MNcsq2B2oW+KKxYy'
+    b'jUAA/eEYz+zs4K519ezrCFJf5sedPpXRaJJYysTvcfClh3bx5/e6ULwOhK4grdNUc6qYhf2y'
+    b'LMmkU1GEsL1MPIU0LO6+rp4f//0i9rQHeWVPDxfMLWDt4jISKROnrtIXjvHczg7+Ze28KRLQ'
+    b'wA4oQgg27O3hysVlCKClb5QNe4/ztUuqKctzk+NxkBiPc/5/v82HzUF0vxvDsmzmEwbStJgx'
+    b'fErAlAiXhupQMS2JJSWWKVGcGqoTfvLcIY4HYzxzx3K8To0jJ8K2+agKlpQU+93ML/PzamMP'
+    b'VzdUZDRGm3zT2BHC41CpLc7m/7Ycw+fWuX11Nd/fcJAf3nAOPrfG2h+9x4ctIXS/C8O0kKbE'
+    b'79V4df0FlOV5SRq2XzdO8d1Jw6JreILd7SGe3tZFT18ExevASq+xLIkE9Fw3z25qQ1cFj992'
+    b'HuX5HgBShsnutiBzCn1ccXYJv3jjCEPjcQqzXEhAWJaUQsCvNjZzy0VVHDo+gmFKUqbkzQN9'
+    b'rFkwi8sXlnDdL7fz8tZudL+LlGFlVEOmLL66ejY1AR/RhImuKvg9evq+YFGFnxU1+ShCMB5P'
+    b'ceuje3hhazeKV8+AmCRdVUiFY/zolsV85wv1APzPhoOU5nrI9WpctrCY9sEIh3tHufH82ZiW'
+    b'RBMCmo6H8XscZLt1/B4HL7zfTX1pDqV5blbPL+axd4/x8tYutBx3hvlTFfyvu3u5ZmUZQgiG'
+    b'R+O88E4nSFBUgQWsWhLgT99eSZ7PyXN3rmB4PMHm/QOoHn1KpDUsC5Hl5J7nD3LZ2QFqinz4'
+    b'nSq3XlLFxv297DgyyLolZWw5PMBoLEWOW0cBaOwMsbymAIC6khz+99qziSUNVtYUEk0arH/u'
+    b'ILgdmNZU5hUhIGVSW+nnoZsbePCmpdx3/UKcfheq34V02Dq/9d0u7nhsj40Xwfqr6kERfDxL'
+    b'kBLbsA3JXU98SLbHwfwKP79+s5lNTf2cO9fmsb40h73tQduIJxIGE3GD2uJsAMZjSbxOB/90'
+    b'wRxURbD++Y8YGo6h5dh6Px1N0XlTkjQtJIKV9QX4XBopUxKTMB5PkuVysHR2HsWFHvqCMduL'
+    b'nQLEtCSqR2froUFe2XucqxvKqA5kE8hx43bYHnFJZR5/auxh9fwAWvOJUQI5LgBiSYMv/243'
+    b'v765gdJcD6FIgie3d4FbP+30p2jRKe81VaSlAz/50gJqi7OJJU2EAIdqM+BzqeR4dPqG0sGP'
+    b'07IZEIKH3zrG1Q1lnFXgYzIAW1JSmO1CRTA8nkBrHxinriQn/XCF3355GTluBwBvH+qnt38C'
+    b'1ef41KxwkiaDjCVh1X1bIe3/c5wqR376OQJ+O/UITSRBnZ55y5Lg0ni3eZijfWPUFGcTiiQY'
+    b'jaXY3TbMtcsqKM/30DYwjjISSVKa58kAKMp2o6azxI0f9YOc2b3PTAIhBBKBoijI8RQrq/MJ'
+    b'+O3n7GgZZnAoiqKrnJ5K2mFDUwXJ8STbWoYwLJNfbGzmwbdakVKiqwoluR56QxMoKdMiz+fk'
+    b'2Z0dPLW9g5f3dHOkdwSAD9pHQFdOM7ZPJSmRMQPiBqlQjOJiLw/ctBiASDzF9/7YdDIXmHEP'
+    b'QAi2tQyjKSrf+Xw9l84rIp40aekbpSTXzUgkiTZ52mOxFHvaR4gmDO69/mzCE0m6QzHQlOl1'
+    b'9BOeq6kKC+ty0VXB+TX5/MeVdRRluxiLJfnSb3bx4dHglGA20z5ogqZeu654ansHw5Ek5Xke'
+    b'FCHIcuskTAttsqi4qG4WiyryONwbRlME4WiS8VgKFIXPwD+WJXHpCk/c1sBZ+V4My6JzKMoD'
+    b'b7TwyBtHGRtNoue6To8nHwcgAUVhOJIkZZp85aJqNFXJCM6w7KivaZp9ZW5xNqoimFeajdeh'
+    b'cqh3HCwQKtPq6UykKoJI3GDZ995BVxViSYNUJMnF5wRYf/0Cfv9OO0e7xlB9+ic6Bpk2vljK'
+    b'IpGS+FyqXSli526aooAAZfJ0TUuSMi2y3Tq6pmLJT0jOPo0ExBIm4zEDUwpwaORlObl7XS0f'
+    b'/vByrlxegjmRnLFM/NhW6RRcsulgH7uPDadbO/Z9xTDTLQxFoKsKb+zv5XDvKIFsF0K1F/5N'
+    b'OBQBpoWVMFAVwYa32vnW4414nRrP37mCpXPzMOMGyky1LgIkeBwqHofGy3u6aTkxxr7OEB+0'
+    b'DSPs7VGShglA13CE2//wAbuOBZlXko3XqZLndYD52RAI0imBgGfuPJe991/OH/91BY4iL8/s'
+    b'6KYvHMXr1Pn+9Qsmu2XT7yMA06Is1w6y0ViKOy+v5dpl5ZwITdgRW4AyqYeDo3ECOS5m5Th5'
+    b'93Affq+TOQUeuwz8LO0+kfaASM6dncfSylzOm5OHS1cYCSc42D0KwOr5Aead5cdKGNM2sCYB'
+    b'1JfYKU5dmZ/vvPAhP33tCEurCghGErh1Fc2lq/SHY6yoKWRFTSETCYOh8TgAK+fms+fwEIrQ'
+    b'+JSia1oy0opqTArRtOgcjgLg1FWWV+dxpG0ExcW0iR1CsKquEICG2fnMDWQhJeT5nOxtC1KQ'
+    b'7UIpzHbRE5wAIGFYuB0qFenWyd+dUwza3xDI0qSmT1YVpHXLrnknqTrgYzojEwJMwyI318VF'
+    b'9UWZa7leJzkeO805HpqgPN+LUlOcRdtgBABdtfsIT+1o542PelmzsJj6ihys+PRi/lvo1KTQ'
+    b'73FMa1+KIiBusG5RgGK/mz9+0EXT8XAGCMDgWIKqWT6U2uIcguMJAHa2DvHdlz6iIMvFsQEb'
+    b'1J1XVEPSnNFbTEeqImwXmflK+rMiphxExo1+TMKWBegK31o7F5AEIwnueekA333xIxQh6A5O'
+    b'oGsKPpeOoiqCwhwXB3vCzCny8YUlpZiWpGt4gsaOILevqWF+lR8jljojEJYlMSdSGBOpTKog'
+    b'pX2NSBIjaWbWThcgNVVBRpLceEEF51bl0zYQ4Wj/OOuvms8tF80BoLEjRH2pnUErAMurCtje'
+    b'OkhJrofGjhB+r4NrlpXz4u4uAH5202KQEjGdR5Wgn8KJz6WxpqGENQ0leF1269XlUFm9tJjl'
+    b'K8upKsnKrNUVbOtNS0VRBEbSpKjAxY/+YREg2dzUj6IoPLmtg+pZvnSTIMKS2XkACNOypCIE'
+    b'j7xzlDULApT4XfxyYwtuh8a6c0qYU+RDUxTueekAP3j2ILrfTSpdmamKwJxI8tvbG7h9dQ2G'
+    b'ZaEpkxabRmc/ZqqU0p2QntAEi9a/xch4KtMYM2NJNtx9IV9sKCNpWOiqXXoeOh5mXkk221qG'
+    b'iCUNrlxchmnJk12J9sEImw6e4JaLqmgbiFBfmpMWsSSWMvE4NG56+H2e2tiGnpvuCaX5qyv2'
+    b'oSoiM5iYtFPllOb95LXJJpeU4NQU2oeijMcNNCEwxhP87Nal3LW2loHRGLNy3Bim3Q8VAqJJ'
+    b'k4ffbuGOy2ozbUjNrp4kc4p8ZLl09rQFuaC2KDNdSaRMntzWzoW1RTx5+3KEEDz5xjG0HBdS'
+    b'sXOo5q7R05X5TEiC6tJQLImRSPHArUu4a20tAN998QBXN5Rx5eLSDC+v7O1mWVUBLl3NSFGB'
+    b'k6Oca8+tYNfRYYbHE+iqwoHuEIYF/7yqilf39SKBJ247j3tvOQczaWBGU6iqQHdrNiPOM3up'
+    b'Lg2HR0fz6JiRJFkulZfuvoB/W1vHtpYB/rC1jevOK2PLkQGe3dmBrirs7woxGktxYW3RlAZv'
+    b'RshSSly6yueXlPLU9nYAmnpH2d8VYl9nyC7Msdvq91w1n533rmZJXT7meDLjcRQFTnrGk38g'
+    b'EbYXtYOUKUmOJTBiKa5eVc6hn1zBdcvs9vmyOfkkDYuntnbwzcvmsqKmkPF4ijcP9HHj+bPT'
+    b'ajhNd9rWU4miCDY39dMdnOCWVVVsPtRH+9AEX2wopyDLiZSS95oHOa+qALdD5eU9Pfxi41He'
+    b'bw3arlIRoCknubVPx+6NGhYSiSvHydpFAf59XS3n1xTQHZzAsiSVhb7M6T6/q5PlNQVUFvh4'
+    b'4LXDXL20nOpA1szt9UmaHCL8ubGHSNzgxpWzp4xYT4Rj/Ofz+7m4vpC5gWxW1BSiCEHT8TCb'
+    b'Dg2wvXWYY/0RhsaTxNO9Uo9DJeB3Mq8km4vrCvncomJK/G6ShsmO1iHeax7EtODe687GtCRS'
+    b'2kV9yrR4cFMLF8+bxeLKvNOYnxbAqSBe3XecgdEYX724GtJj1vv/0sSsbBf9ozHC0RT/fc1C'
+    b'3Lo2pUaXSKIJk3jKVjuPU8WlaxlhRBIpfE6daNLgQE+YEyNRHnyzle9du4BL5gUAu53/6JZj'
+    b'rK4PsLgyb+Y52aeNV7c2D8ifv94kRyYSMp405Au7OqWUUn7ziQ/k+8eGM+u3tQzIH/+lSb60'
+    b'uzMzSpVSZsaoI5G4/N07rbJtYEze9uj7U9Z0Do3L37x5RL55oFdKKWXzibC8/9VD8mj/2BRe'
+    b'pqMZx+pqesR5YW0R5fkefr/lGOdWFXDD8rMIR5M0zM7j3Dn5mYHciZEYPcMTLK7w26JFMDgW'
+    b'x+NQ8bl0/F4n4XF74O3UVXpHopTmeuzRgSW5YXklRdkuXt7TzeBonK+vqSHb7cCSM08o4RQv'
+    b'NBMIS0oqC3x8+4o6guMJfrWxmf5wjJsvrAJx0iNYUtI9GMl0+YSAH/65ibeb+jP75XodDIbj'
+    b'rFtcwvtHh+zvWZI5RVl0DkV44LXDzMpx8fU1c8lyOabV+Y/TGf3U4FQjHhqLs+lgH6OxJIsq'
+    b'8lhUkYvPpRFJpGhsD9IwpwCv0xZsPGWiKQIt3boZHItjWva0RUpJaCLJ3vYgLf1jVBb4WLMg'
+    b'gMehYVnp30ucQQJ8xj/2gKm/mRiZSLKnfZjuYBSHplBZ4KUmkI3boeJ1apkh9iTFUyaxpMHA'
+    b'WILOoQj94RiWhNriLJbOzsOla1MO6kzpMwEAWxof18u+cIzWvjF6QlEmEkam3yMy1bFNuirw'
+    b'exxUFHiZG8gi12sPxyW2Kp1Jm+Xj9P+8ZNC/YXj7uwAAAABJRU5ErkJggg==')
 
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
+        self.setWindowIcon(QtGui.QIcon('orig.ico'))
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(717, 519)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -178,7 +270,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("A framework for search, analysis and visualization of social networks data", "A framework for search, analysis and visualization of social networks data"))
         self.table_of_contents_3.setText(_translate("MainWindow", "Блок ввода"))
         self.radioButton_3.setText(_translate("MainWindow", "Vk"))
         self.radioButton_4.setText(_translate("MainWindow", "Facebook"))
