9861453447cab7c1e967dedd8c24926f9eba08c2
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2020-05-23 19:12:35 +0300

    Add files via upload

--- vk.py
+++ vk.py
@@ -1,5 +1,6 @@
 import vk_api
 import json
+import time
 code = '2f44f3122f44f3122f44f312422f34e2f922f442f44f312711a582d5fc2a27b99d64bf4'
 app = 7344619
 secret = 'GJdQphxS7yjBShLWemze'
@@ -7,7 +8,41 @@ vk_session = vk_api.VkApi(app_id=app, client_secret=secret, token=code)
 vk = vk_session.get_api()
 
 
-def BFS (friends, q, used, id, count, current):
+def findWay(way, queue, used, ancestor, idFinale, finalFriends, time_limit):
+    start_time = time.time()
+    while queue:
+        if time.time() - start_time > time_limit:
+            return
+        person = queue.pop(0)
+        if person[0] in used:
+            continue
+        used.add(person[0])
+        info = vk.users.get(user_ids=person[0], fields='is_closed')
+        if not ('deactivated' in info[0]) and not (info[0]['is_closed']):
+            response = vk.friends.get(user_id=person[0])
+            if person[0] in ancestor:
+                continue
+            else:
+                ancestor[person[0]] = person[1]
+            intersection = set(response['items']).intersection(finalFriends)
+            if intersection:
+                intersection = str(list(set(response['items']).intersection(finalFriends))[0])
+                ancestor[person[0]] = person[1]
+                ancestor[intersection] = person[0]
+                ancestor[idFinale] = intersection
+                current = idFinale
+                while current:
+                    way.append(current)
+                    current = ancestor[current]
+                return
+            for id in response['items']:
+                if not(str(id) in used):
+                    queue.append((str(id), person[0]))
+
+
+def BFS (friends, q, used, id, count, current, start_time, time_limit):
+    if time.time() - start_time > time_limit:
+        return
     info = vk.users.get(user_ids=id, fields='is_closed')
     if not('deactivated' in info[0]) and not(info[0]['is_closed']):
         response = vk.friends.get(user_id=id, fields='bdate, sex')
@@ -39,42 +74,64 @@ def BFS (friends, q, used, id, count, current):
                 q.append(resp['id'])
         current = len(friends) - 1
         if current < count:
-            BFS(friends, q, used, q.pop(0), count, current)
+            BFS(friends, q, used, q.pop(0), count, current, start_time, time_limit)
         else:
             return
     else:
-        BFS(friends, q, used, q.pop(0), count, current)
+        BFS(friends, q, used, q.pop(0), count, current, start_time, time_limit)
 
 
-user_id = input()
-count = int(input())
-used = set()
-q = []
-q.append(user_id)
-friends = []
-start_info = vk.users.get(user_ids=user_id, fields='bdate, sex')
-start = dict()
-if not ('deactivated' in start_info[0]) and not (start_info[0]['is_closed']):
-    start['id'] = start_info[0]['id']
-    start['last_name'] = start_info[0]['last_name']
-    start['first_name'] = start_info[0]['first_name']
-    if start_info[0]['sex'] == 2:
-        start['sex'] = 'male'
-    elif start_info[0]['sex'] == 1:
-        start['sex'] = 'female'
+type = int(input())
+if type == 1:
+    user_id = input()
+    count = int(input())
+    time_limit = int(input())
+    used = set()
+    q = []
+    q.append(user_id)
+    friends = []
+    start_info = vk.users.get(user_ids=user_id, fields='bdate, sex')
+    start = dict()
+    if not ('deactivated' in start_info[0]) and not (start_info[0]['is_closed']):
+        start['id'] = start_info[0]['id']
+        start['last_name'] = start_info[0]['last_name']
+        start['first_name'] = start_info[0]['first_name']
+        if start_info[0]['sex'] == 2:
+            start['sex'] = 'male'
+        elif start_info[0]['sex'] == 1:
+            start['sex'] = 'female'
+        else:
+            start['sex'] = 'unknown'
+        if 'bdate' in start_info[0]:
+            start['bdate'] = start_info[0]['bdate']
+        else:
+            start['bdate'] = 'unknown'
+        start_friends = vk.friends.get(user_id=start['id'])
+        start['friends'] = start_friends['items']
+        friends.append(start)
+        used.add(start['id'])
+        start_time = time.time()
+        BFS(friends, q, used, q.pop(0), count, 0, start_time, time_limit)
+        f = open('friends.json', 'w')
+        f.write(json.dumps(friends, indent=4))
+        f.close()
     else:
-        start['sex'] = 'unknown'
-    if 'bdate' in start_info[0]:
-        start['bdate'] = start_info[0]['bdate']
-    else:
-        start['bdate'] = 'unknown'
-    start_friends = vk.friends.get(user_id=start['id'])
-    start['friends'] = start_friends['items']
-    friends.append(start)
-    used.add(start['id'])
-    BFS(friends, q, used, q.pop(0), count, 0)
-    f = open('friends.json', 'w')
-    f.write(json.dumps(friends, indent=4))
-    f.close()
-else:
-    print('Closed account')
+        print('Closed account')
+elif type == 2:
+    idStart = input()
+    idFinale = input()
+    time_limit = int(input())
+    used = set()
+    ancestor = dict()
+    way = []
+    finalFriends = []
+    queue = [(idStart, None)]
+    info = vk.users.get(user_ids=idFinale, fields='is_closed')
+    if not ('deactivated' in info[0]) and not (info[0]['is_closed']):
+        response = vk.friends.get(user_id=idFinale)
+        for resp in response['items']:
+            finalFriends.append(resp)
+        findWay(way, queue, used, ancestor, idFinale, finalFriends, time_limit)
+        f = open('way.json', 'w')
+        f.write(json.dumps(way, indent=4))
+        f.close()
