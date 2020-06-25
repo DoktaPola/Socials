310ab60bee5b4c0e254691fc87d5bdea47591aa3
andryjkost <61208923+andryjkost@users.noreply.github.com>
2020-06-25 23:33:54 +0300

    Окончательный вариант

--- main.py
+++ main.py
@@ -1,126 +1,89 @@
 import sys  # sys нужен для передачи argv в QApplication
 from PyQt5 import QtWidgets
-import main_design  # Это наш конвертированный файл дизайна
+import design_final  # Это наш конвертированный файл дизайна
 from PyQt5.QtCore    import QUrl
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QWidget, QMessageBox, QApplication,QProgressBar
-import new_lib_leg
-class ExampleApp(QtWidgets.QMainWindow, main_design.Ui_MainWindow):
+import graph_new_lib
+import vk
+import functional
+class Windowed_application(QtWidgets.QMainWindow, design_final.Ui_MainWindow):
     def __init__(self):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
 
-    def initUI(self):
-        self.comboBox_facebook_search.activated[str].connect(self.function_selection_facebook)
-        self.pushButton_facebook_offer_friends.clicked.connect(self.offer_friends)# подключаем к функциям кнопки
-        self.buttom.clicked.connect(self.offer_friends)
-        self.pushButton_facebook_find(self.find_facebook)
-        self.pushButton__facebook_search(self.facebook_search)
-        self.pushButton_vk_find_1(self.vk_find)
+    def facebook_group_search(self):
+        graph_new_lib.draw_groups(graph_new_lib.find_groups())
+        url = 'D:/inter/facebook_net.html'
+        self.frame_2.load(QUrl(url))
+        url1 = 'http://127.0.0.1:8889/'
+        self.frame_legend.load(QUrl(url1))
 
-        # self.fig = functional.draw_social_groups()
-        # self.mda = MyMplCanvas(self.fig)
-        # vbox = QVBoxLayout()
-        # vbox.addWidget(self.buttom_vk)
-        # vbox.addWidget(self.buttom)
-        # vbox.addWidget(self.mda)
-        # self.setLayout(vbox)
-
-    def offer_friends(self):
-            new_lib_leg.find_by_info('study', self.lineEdit_facebook_search.text())
-            url = 'D:/inter/facebook_net.html'
-            self.frame_2.load(QUrl(url))
+    def facebook_offers_friends(self):
+        graph_new_lib.draw_common_friends_between_friends(graph_new_lib.find_common_friends_between_friends())
+        url = 'D:/inter/facebook_net.html'
+        self.frame_2.load(QUrl(url))
+        url1 = 'http://127.0.0.1:8889/'
+        self.frame_legend.load(QUrl(url1))
 
     def facebook_search(self):
-        if self.comboBox_facebook_search.currentText()=="place of study":
-            new_lib_leg.find_by_info('study', self.lineEdit_facebook_search.text())
+        if self.comboBox_facebook_search.currentText() == "place of study":
+            graph_new_lib.find_by_info('study', self.lineEdit_2.text())
             url = 'D:/inter/facebook_net.html'
             self.frame_2.load(QUrl(url))
-            url='http://127.0.0.1:8889/'
+            url = 'http://127.0.0.1:8889/'
             self.frame_legend.load(QUrl(url))
 
 
         elif self.comboBox_facebook_search.currentText() == "residence":
-            new_lib_leg.find_by_info('live in', self.lineEdit_facebook_search.text())
+            graph_new_lib.find_by_info('live in', self.lineEdit_facebook_search.text())
             url = 'D:/inter/facebook_net.html'
             self.frame_2.load(QUrl(url))
             url = 'http://127.0.0.1:8889/'
             self.frame_legend.load(QUrl(url))
         else:
-            new_lib_leg.find_by_info('b-day', self.lineEdit_facebook_search.text())
+            graph_new_lib.find_by_info('b-day', self.lineEdit_facebook_search.text())
             url = 'D:/inter/facebook_net.html'
             self.frame_2.load(QUrl(url))
             url = 'http://127.0.0.1:8889/'
             self.frame_legend.load(QUrl(url))
 
-    # def Activated(self):
-    #         self.fig.clf()
-    #         self.mda = MyMplCanvas(self.fig)
-    # #
-    # def offer_friends(self):
+    def facebook_find_path(self):
+        friends_queue = graph_new_lib.deque()
+        FB = graph_new_lib.create_graph()
+        path = graph_new_lib.find_connection(FB, self.lineEdit_facebook_find_1.text(),
+                                             self.lineEdit_facebook_find_2.text(), friends_queue)
+        url = 'D:/inter/facebook_net.html'
+        self.frame_2.load(QUrl(url))
+        url = 'http://127.0.0.1:8889/'
+        self.frame_legend.load(QUrl(url))
 
-    # # self.fullScreenButton.clicked.connect(self.swichFullScreen)
-    # # def swichFullScreen(self):
-    # #     #        self.setLayout(self.findLayout("full"))           # ---
-    # #     #        self.show()                                       # ---
-    # #     # +++ vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-    # #     if self.sender().text() == "Full":
-    # #         self.topLeftBox.hide()
-    # #         self.topRightBox.hide()
-    # #         self.bottomLeftBox.hide()
-    # #         self.bottomRightBox.hide()
-    # #         self.mainLayout.addWidget(self.bottomRightBox, 0, 0, 1, 2)
-    # #         self.bottomRightBox.show()
-    # #         self.fullScreenButton.setText("NoFull")
-    #
-    #     else:
-    #         self.bottomRightBox.hide()
-    #         self.topLeftBox.show()
-    #         self.topRightBox.show()
-    #         self.bottomLeftBox.show()
-    #         self.mainLayout.addWidget(self.bottomRightBox, 2, 1)
-    #         self.bottomRightBox.show()
-    #         self.fullScreenButton.setText("Full")
-    # def initUI(self):
-    #     self.browser = QWebEngineView()
-    #
-    #     # Загрузка локальной страницы
-    #     # Полный путь к index.html
-    #     url = 'D:/inter/facebook_net.html'
-    #     self.browser.load(QUrl(url))
-    #     self.widget(self.browser)
-    # def startFunction(self, event):
-    #
-    #     progressbar = QMessageBox.question(self, '',
-    #                                  "Information is being collected, please wait.")
-    #     progressbar.pbar = QProgressBar(self)
-    #     progressbar.pbar.setGeometry(30, 40, 200, 25)
-    #     self.btn = QtWidgets.QPushButton('Cancel', self)
-    #     self.btn.move(40, 80)
-    #     self.btn.clicked.connect(self.doAction)
-    #     if reply == QMessageBox.Yes:
-    #         event.accept()
-    #     else:
-    #         event.ignore()
+    def vk_search_social_circle(self):
+        vk.main(1, self.lineEdit_11.text(), self.lineEdit_2.text(), self.lineEdit_work_time.text(), 0, 0, 0)
+        functional.main(1)
+        url = 'friends.html'
+        self.frame_3.load(QUrl(url))
 
-    def closeEvent(self, event):
+    def vk_grouping_func(self):
+        vk.main(3, 0, 0, self.lineEdit_work_time.text(), 0, 0, self.lineEdit_2.text())
+        functional.main(3)
+        url = 'groups.html'
+        self.frame_3.load(QUrl(url))
 
-        reply = QMessageBox.question(self, 'Message',
-                                     "Are you sure to quit?", QMessageBox.Yes |
-                                     QMessageBox.No, QMessageBox.No)
-
-        if reply == QMessageBox.Yes:
-            event.accept()
-        else:
-            event.ignore()
-
+    def vk_find_path(self):
+        vk.main(2, 0, 0, self.lineEdit_work_time_2.text(), self.lineEdit_vk_find_1.text(),
+                self.lineEdit_vk_find_2.text(), 0)
+        functional.main(3)
+        url = 'way.html'
+        self.frame_3.load(QUrl(url))
+ 
 
 def main():
+    graph_new_lib.create_graph()
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
-    window = ExampleApp()  # Создаём объект класса ExampleApp
-    # window.setWindowOpacity(0.95)
+    window = Windowed_application()  # Создаём объект класса ExampleApp
     window.setWindowIcon(QIcon('orig.ico'))
     window.show()  # Показываем окно
     app.exec_()  # и запускаем приложение
